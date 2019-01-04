c820ddfaed9f12a2a03507df630f286cc4a03919
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 22:46:22 -0800

    add another test to NOT equal

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions');
 test('Adds 2 and 3 to equal 5', () => {
   expect(functions.add(2, 3)).toBe(5);
 });
+
+test('Adds 2 and 3 to NOT equal 4', () => {
+  expect(functions.add(2, 4)).not.toBe(4);
+});
